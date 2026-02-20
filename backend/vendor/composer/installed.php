3aff2ff07eeb75b293e8766e0779ecf1c64d8af7
Marjan ghazi <syedmarjanghazi@gmail.com>
2026-02-20 13:00:25 +0500

    first commit

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '97c665b125cb1d0e438ed32ce4cd8a54f87f8db1',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '97c665b125cb1d0e438ed32ce4cd8a54f87f8db1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
